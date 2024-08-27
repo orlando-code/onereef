7a7e79551858b7d740919196bd9ccf8f30c59981
Orlando Timmerman <rt582@cam.ac.uk>
2024-08-27 09:35:44 -0300

    investigating tif file locally

--- gee_utils.py
+++ gee_utils.py
@@ -1,4 +1,9 @@
 import ee
+
+# general
+import numpy as np
+
+# plotting
 import folium
 
 
@@ -30,3 +35,31 @@ def landmask(img):
     classified = ndwi.classify(trained)
     mask = classified.eq(1)
     return img.updateMask(mask)
+
+
+def inspect_tif(tif_object) -> None:
+    print("DIMENSIONS | height: ", tif_object.height, "width: ", tif_object.width)
+    print("BOUNDS | ", tif_object.bounds)
+    print("BANDS | count: ", tif_object.count)
+    print("DATA TYPE(S) | ", {i: dtype for i, dtype in zip(tif_object.indexes, tif_object.dtypes)})
+    print("CRS | ", tif_object.crs)
+    print("TRANSFORM | ", tif_object.transform)
+
+
+def process_tif(tif_object) -> dict:
+    """
+    Processes a tif object and returns a dictionary of bands. Replaces -9999 with np.nan.
+    
+    Parameters:
+    tif_object (rasterio.io.DatasetReader): The tif object to process.
+    Returns:
+    dict: A dictionary of bands (label and array).
+    """
+    # I think b1 = coral, b2 = sand, b3 = algae
+    bands = {}
+    for band in tif_object.indexes:
+        b = tif_object.read(band)
+        b[b == -9999] = np.nan
+        bands[f"b{band}"] = b
+    
+    return bands
